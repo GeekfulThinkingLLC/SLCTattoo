521c14d9fead1f18a2549cf4db05c6b20ada328f
Cody Higgins <cody.higginsdev18@gmail.com>
2017-07-01 21:09:55 -0600

    started buildling out the form_for and action mailer

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,25 @@
 class AppointmentsController < ApplicationController
+
+	def new
+		@appointments = Appointment.new(appointment_params)
+	end
+
+	def create
+		@appointments = Appointment.new(appointment_params)
+		respond_to do |format|
+			if @appointments.save
+				AppointmentMailer.appointment_email(@consultation).deliver!
+				format.html { redirect_to contact_path, notice: "Appointment was submitted to Salt Lake Tattoo Co. Looking forward to chatting!"}
+			else
+				format.html { redirect_to contact_path}
+			end
+		end
+	end
+
+private
+
+	def appointment_params
+		params.require(:appointments).permit(:name, :email, :phone_number, :body, :selected_day, :image)
+	end
 end
+	
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,4 @@
 class Appointment < ApplicationRecord
+
+	validate_presence_of :name, :email, :phone_number, :body, :selected_day
 end
