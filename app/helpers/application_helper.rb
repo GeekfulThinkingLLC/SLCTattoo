6f10b4ad647e4355626b72bc725e3ea5c88d3576
Trevor <trevorhoward2@hotmail.com>
2017-07-01 22:37:09 -0600

    Implemented gritter notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
   def link_fa_to(icon_name, text, link)
     link_to content_tag(:i, text, :class => "fa fa-#{icon_name} fa-icon-padding"), link
   end
+
+  def alerts
+    alert = flash[:alert]
+    error = flash[:error]
+    notice = flash[:notice]
+
+    if alert
+      js add_gritter(alert, title: "Salt Lake Tattoo Co", sticky: false, image: :warning, time: 3000)
+    elsif error
+      js add_gritter(error, title: "Salt Lake Tattoo Co", sticky: false, image: :error)
+    else notice
+      js add_gritter(notice, title: "Salt Lake Tattoo Co", sticky: false, image: :success, time: 6000)
+    end
+    
+  end
 end
